a145365d6d8df4a9db6cc8f60c92a0e3513de1e8
waldo <evgenijtser@gmail.com>
2024-03-26 22:14:10 +0300

    Jsdoc Employee

--- src/entities/Employee/model/types/employee.ts
+++ src/entities/Employee/model/types/employee.ts
@@ -1,11 +1,35 @@
 export type EmployeeRole = 'driver' | 'waiter' | 'cook';
 
+/**
+ * @category entities
+ * @description Модель сотрудника
+ */
 export interface Employee {
+    /**
+     * Id текущего сотрудника
+     */
     id: number;
+    /**
+     * ФИО текущего сотрудника
+     * @example Тестовый Тест Тестович
+     */
     name: string;
+    /**
+     * Находится ли текущий сотрудник в архиве
+     */
     isArchive: boolean;
+    /**
+     * Роль текущего сотрудника - {@link EmployeeRole}
+     */
     role: EmployeeRole;
+    /**
+     * Номер телефона сотрудника
+     * @example +7(999)999-99-99
+     */
     phone: string;
+    /**
+     * Дата рождения сотрудника
+     * @example 12.03.1987
+     */
     birthday: string;
-
 }
