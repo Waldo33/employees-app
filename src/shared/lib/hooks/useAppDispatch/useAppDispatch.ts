cc9b0bfa01ab62f6bf26d5583665e0a633065c9f
waldo <evgenijtser@gmail.com>
2024-03-26 21:40:22 +0300

    Add jsdoc for test

--- src/shared/lib/hooks/useAppDispatch/useAppDispatch.ts
+++ src/shared/lib/hooks/useAppDispatch/useAppDispatch.ts
@@ -1,4 +1,10 @@
 import { AppDispatch } from 'app/providers/StoreProvider';
 import { useDispatch } from 'react-redux';
 
+/**
+ * Типизированный {@link https://redux-toolkit.js.org/usage/usage-with-typescript#getting-the-dispatch-type | useDispatch}
+ * @category shared
+ * @function useAppDispatch
+ * @returns {AppDispatch}
+ */
 export const useAppDispatch: () => AppDispatch = useDispatch;
